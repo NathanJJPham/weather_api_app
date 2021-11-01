6d96765da0f603a0da3ff00bbe537f4287a53bf5
NathanJJPham <nathan.pham@kin.com>
2021-11-01 17:24:51 -0400

    Map through days and display weather stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,11 @@ class HomeController < ApplicationController
   API_KEY = ENV["API_KEY"]
 
   def index
-      @current_weather = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=london&appid=#{API_KEY}")
+      @current_weather = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=london&appid=#{API_KEY}&units=imperial")
       @lon = @current_weather['coord']['lon']
       @lat = @current_weather['coord']['lat']
 
-      @forecast_weather = HTTParty.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{@lat}&lon=#{@lon}&exclude=&appid=#{API_KEY}")
+      @forecast_weather = HTTParty.get("https://api.openweathermap.org/data/2.5/onecall?lat=#{@lat}&lon=#{@lon}&exclude=&appid=#{API_KEY}&units=imperial")
       
   end
 
